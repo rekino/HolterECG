f94c358db41dba98c42f8c58b2ebaa1d0470beb0
Ramin Barati <rekino@gmail.com>
2017-10-26 14:27:19 +0330

    - made all charts non animating - BarReport is now MVVM -BarReport first draft

--- HolterECG/Controls/BarReport.xaml.cs
+++ HolterECG/Controls/BarReport.xaml.cs
@@ -18,29 +18,19 @@ using System.Windows.Shapes;
 
 namespace HolterECG.Controls
 {
-    /// <summary>
-    /// Interaction logic for BarReport.xaml
-    /// </summary>
-    public partial class BarReport : UserControl
+    class BarReportModel:System.ComponentModel.INotifyPropertyChanged
     {
         State _state;
-        SeriesCollection _seriesSystolic;
-        SeriesCollection _seriesDiastolic;
-        SeriesCollection _seriesHeartRate;
-        public BarReport()
+        public BarReportModel()
         {
-            InitializeComponent();
-            _state = Application.Current.FindResource("state") as State;
-            _seriesSystolic = new SeriesCollection();
-            _seriesDiastolic = new SeriesCollection();
-            _seriesHeartRate = new SeriesCollection();
-            chartSys.Series = _seriesSystolic;
-            chartDia.Series = _seriesDiastolic;
-            chartHr.Series = _seriesHeartRate;
-        }
+            this._state = Application.Current.FindResource("state") as State;
 
-        private void UserControl_Loaded(object sender, RoutedEventArgs e)
-        {
+            this.Systolic = new SeriesCollection();
+            this.Diastolic = new SeriesCollection();
+            this.HeartRate = new SeriesCollection();
+
+            this.YFormatter = value => String.Format("%{0}", value * 100);
+
             var totalBuckets = 10;
             List<double> syss = new List<double>();
             List<double> dias = new List<double>();
@@ -53,17 +43,16 @@ namespace HolterECG.Controls
                 hrs.Add(reading.HR);
             }
 
-            _seriesSystolic.Add(CreateHistogram(syss, totalBuckets, "Systolic Blood Pressure"));
-            _seriesDiastolic.Add(CreateHistogram(dias, totalBuckets, "Diastolic Blood Pressure"));
-            _seriesHeartRate.Add(CreateHistogram(hrs, totalBuckets, "Heart Rate"));
+            Systolic.Add(CreateHistogram(syss, totalBuckets, "Systolic Blood Pressure", Brushes.Red));
+            Diastolic.Add(CreateHistogram(dias, totalBuckets, "Diastolic Blood Pressure", Brushes.Green));
+            HeartRate.Add(CreateHistogram(hrs, totalBuckets, "Heart Rate", Brushes.Blue));
         }
-
-        ColumnSeries CreateHistogram(IEnumerable<double> data, int totalBuckets, string title)
+        ColumnSeries CreateHistogram(IEnumerable<double> data, int totalBuckets, string title, Brush fill)
         {
             var min = data.Min();
             var max = data.Max();
             var buckets = new double[totalBuckets];
-            var unit = 100.0 / data.Count();
+            var unit = 1.0 / data.Count();
 
             var bucketSize = (max - min) / totalBuckets;
             foreach (var value in data)
@@ -77,11 +66,31 @@ namespace HolterECG.Controls
                         bucketIndex--;
                     }
                 }
-                buckets[bucketIndex] += unit ;
+                buckets[bucketIndex] += unit;
             }
-            ColumnSeries series = new ColumnSeries { Title = title };
+            ColumnSeries series = new ColumnSeries { Title = title, Fill = fill };
             series.Values = new ChartValues<double>(buckets);
             return series;
+        }
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+        }
+        public SeriesCollection Systolic { get; private set; }
+        public SeriesCollection Diastolic { get; private set; }
+        public SeriesCollection HeartRate { get; private set; }
+        public Func<double, string> YFormatter { get; private set; }
+    }
+    /// <summary>
+    /// Interaction logic for BarReport.xaml
+    /// </summary>
+    public partial class BarReport : UserControl
+    {
+        public BarReport()
+        {
+            InitializeComponent();
         }
     }
 }
