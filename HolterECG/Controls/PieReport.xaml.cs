26c6fb105d35d1b1b588c6bf097347bdccb3facf
Ramin Barati <rekino@gmail.com>
2017-11-12 15:58:09 +0330

    - Changed PieReport to second design

--- HolterECG/Controls/PieReport.xaml.cs
+++ HolterECG/Controls/PieReport.xaml.cs
@@ -58,6 +58,13 @@ namespace HolterECG.Controls
                 OnPropertyChanged("Title");
             }
         }
+        private Brush _fill;
+
+        public Brush Fill
+        {
+            get { return _fill; }
+            set { _fill = value; }
+        }
 
 
         public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
@@ -79,32 +86,75 @@ namespace HolterECG.Controls
             this.Diastolic = new SeriesCollection();
             this.HeartRate = new SeriesCollection();
 
-            this.SystolicRanges = new ObservableCollection<Range>();
-            this.SystolicRanges.Add(new Range { Low = 50, High = 80, Title="Under Normal" });
-            this.SystolicRanges.Add(new Range { Low = 80, High = 110, Title = "Normal" });
-            this.SystolicRanges.Add(new Range { Low = 110, High = 140, Title = "High Normal" });
-            this.SystolicRanges.Add(new Range { Low = 140, High = 170, Title = "Grade I" });
-            this.SystolicRanges.Add(new Range { Low = 170, High = 190, Title = "Grade II" });
-            this.SystolicRanges.Add(new Range { Low = 190, High = 210, Title = "Grade III" });
+            this._sysMin = 80;
+            this._sysMax = 130;
+            this._diaMin = 70;
+            this._diaMax = 90;
+            this._hrMin = 60;
+            this._hrMax = 90;
 
-            this.DiastolicRanges = new ObservableCollection<Range>();
-            this.DiastolicRanges.Add(new Range { Low = 50, High = 80, Title = "Under Normal" });
-            this.DiastolicRanges.Add(new Range { Low = 80, High = 110, Title = "Normal" });
-            this.DiastolicRanges.Add(new Range { Low = 110, High = 140, Title = "High" });
+            this.PropertyChanged += PieReportModel_PropertyChanged;
 
-            this.HeartRateRanges = new ObservableCollection<Range>();
-            this.HeartRateRanges.Add(new Range { Low = 50, High = 80, Title = "Low" });
-            this.HeartRateRanges.Add(new Range { Low = 80, High = 110, Title = "Normal" });
-            this.HeartRateRanges.Add(new Range { Low = 110, High = 140, Title = "High Normal" });
-            this.HeartRateRanges.Add(new Range { Low = 140, High = 170, Title = "High" });
+            this.DrawSysSeries();
+            this.DrawDiaSeries();
+            this.DrawHrSeries();
+        }
 
+        public void SetSysDefaults()
+        {
+            this.SysMin = 80;
+            this.SysMax = 130;
+        }
+        public void SetDiaDefaults()
+        {
+            this.DiaMin = 70;
+            this.DiaMax = 90;
+        }
+        public void SetHrDefaults()
+        {
+            this.HrMin = 60;
+            this.HrMax = 90;
+        }
+        void PieReportModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "SysMax" || e.PropertyName == "SysMin")
+                this.DrawSysSeries();
+            if (e.PropertyName == "DiaMax" || e.PropertyName == "DiaMin")
+                this.DrawDiaSeries();
+            if (e.PropertyName == "HrMax" || e.PropertyName == "HrMin")
+                this.DrawHrSeries();
+        }
+
+        void DrawSysSeries()
+        {
+            List<Range> ranges = new List<Range>();
+            ranges.Add(new Range { Title = "Low", Low = 0, High = this.SysMin, Fill = Brushes.Blue });
+            ranges.Add(new Range { Title = "Normal", Low = this.SysMin, High = this.SysMax, Fill = Brushes.Green });
+            ranges.Add(new Range { Title = "High", Low = this.SysMax, High = double.MaxValue, Fill = Brushes.Red });
             double[] syss = _state.ActivePatient.GetSystolicReadings();
+            var series = CreatePieChart(syss, ranges);
+            this.Systolic.Clear();
+            this.Systolic.AddRange(series);
+        }
+        void DrawDiaSeries()
+        {
+            this.Diastolic.Clear();
+            List<Range> ranges = new List<Range>();
+            ranges.Add(new Range { Title = "Low", Low = 0, High = this.DiaMin, Fill = Brushes.Blue });
+            ranges.Add(new Range { Title = "Normal", Low = this.DiaMin, High = this.DiaMax, Fill = Brushes.Green });
+            ranges.Add(new Range { Title = "High", Low = this.DiaMax, High = double.MaxValue, Fill = Brushes.Red });
             double[] dias = _state.ActivePatient.GetDiastolicReadings();
+            this.Diastolic.AddRange(CreatePieChart(dias, ranges));
+        }
+        void DrawHrSeries()
+        {
+            this.HeartRate.Clear();
+            List<Range> ranges = new List<Range>();
+            ranges.Add(new Range { Title = "Low", Low = 0, High = this.HrMin, Fill = Brushes.Blue });
+            ranges.Add(new Range { Title = "Normal", Low = this.HrMin, High = this.HrMax, Fill = Brushes.Green });
+            ranges.Add(new Range { Title = "High", Low = this.HrMax, High = double.MaxValue, Fill = Brushes.Red });
             double[] hrs = _state.ActivePatient.GetHeartRateReadings();
-
-            this.Systolic.AddRange(CreatePieChart(syss, SystolicRanges));
-            this.Diastolic.AddRange(CreatePieChart(dias, DiastolicRanges));
-            this.HeartRate.AddRange(CreatePieChart(hrs, HeartRateRanges));
+            this.HeartRate.AddRange(CreatePieChart(hrs, ranges));
         }
 
         PieSeries[] CreatePieChart(IEnumerable<double> data, IEnumerable<Range> ranges)
@@ -122,7 +172,7 @@ namespace HolterECG.Controls
                         counter[0]++;
                     }
                 }
-                series.Add(new PieSeries { Title = range.Title, Values = new ChartValues<int>(counter), DataLabels = true, LabelPoint=this.SliceLabel });
+                series.Add(new PieSeries { Title = range.Title, Values = new ChartValues<int>(counter), DataLabels = true, LabelPoint=this.SliceLabel, Fill=range.Fill });
             }
             return series.ToArray();
         }
@@ -137,10 +187,49 @@ namespace HolterECG.Controls
         public SeriesCollection Diastolic { get; private set; }
         public SeriesCollection HeartRate { get; private set; }
         public Func<ChartPoint, string> SliceLabel { get; private set; }
+        private double _sysMax;
 
-        public ObservableCollection<Range> SystolicRanges { get; private set; }
-        public ObservableCollection<Range> DiastolicRanges { get; private set; }
-        public ObservableCollection<Range> HeartRateRanges { get; private set; }
+        public double SysMax
+        {
+            get { return _sysMax; }
+            set { _sysMax = value; OnPropertyChanged("SysMax"); }
+        }
+        private double _sysMin;
+
+        public double SysMin
+        {
+            get { return _sysMin; }
+            set { _sysMin = value; OnPropertyChanged("SysMin"); }
+        }
+        private double _diaMax;
+
+        public double DiaMax
+        {
+            get { return _diaMax; }
+            set { _diaMax = value; OnPropertyChanged("DiaMax"); }
+        }
+        private double _diaMin;
+
+        public double DiaMin
+        {
+            get { return _diaMin; }
+            set { _diaMin = value; OnPropertyChanged("DiaMin"); }
+        }
+        private double _hrMax;
+
+        public double HrMax
+        {
+            get { return _hrMax; }
+            set { _hrMax = value; OnPropertyChanged("HrMax"); }
+        }
+        private double _hrMin;
+
+        public double HrMin
+        {
+            get { return _hrMin; }
+            set { _hrMin = value; OnPropertyChanged("HrMin"); }
+        }
+
     }
     /// <summary>
     /// Interaction logic for PieReport.xaml
@@ -150,6 +239,21 @@ namespace HolterECG.Controls
         public PieReport()
         {
             InitializeComponent();
+        }
+
+        private void setSysDefaults(object sender, RoutedEventArgs e)
+        {
+            (grdTop.DataContext as PieReportModel).SetSysDefaults();
+        }
+
+        private void setDiaDefaults(object sender, RoutedEventArgs e)
+        {
+            (grdTop.DataContext as PieReportModel).SetDiaDefaults();
+        }
+
+        private void setHrDefaults(object sender, RoutedEventArgs e)
+        {
+            (grdTop.DataContext as PieReportModel).SetHrDefaults();
         }
     }
 }
